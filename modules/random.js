403186ba2bb96b01c60707eb8605981b89ed794a
ananun <andranik.margaryan@live.com>
2020-03-26 00:51:37 -0700

    Update random.js

--- modules/random.js
+++ modules/random.js
@@ -1,10 +1,10 @@
 //! My version of P5.js random function, it not tested as it should be
-function random (items){
+function random (items, min){
     var item;
     if (Array.isArray(items)) {
         item = items[Math.floor(Math.random()*items.length)];
     }else if (typeof(items) == 'number') {
-        item = Math.floor(Math.random()*items);
+        item = Math.floor(Math.random()*(items - min) + min);
     }
     return item;
 }
